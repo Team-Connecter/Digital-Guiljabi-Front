cf1a9e4984d50499458c0dfe75ac54d6c550416a
bini59 <myuni0234@gmail.com>
2023-08-16 15:19:55 +0900

    chore : 네비게이션 바에서 쓰지 않는 것들 삭제
    - Detail, Reqeust, concat, about등의 페이지 노출 안되도록 삭제

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,10 +12,6 @@ export const Navbar = () => {
             </div>
             <div className="navbar__links">
                 <button onClick={() => navigate("/")}>Home</button>
-                <button onClick={() => navigate("/about")}>About</button>
-                <button onClick={() => navigate("/contact")}>Contact</button>
-                <button onClick={() => navigate("/request")}>Request</button>
-                <button onClick={() => navigate("/detailInfo")}>Detail</button>
                 <button onClick={() => navigate("/my")}>My</button>
                 <button onClick={() => navigate("/search")}>Search</button>
                 <button onClick={() => navigate("/admin")}>Admin</button>
