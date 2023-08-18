4e3c3c3ea19fe48b569b0daffb21c907d10199c9
Yoon Hyeseong <80237040+hsyoonhs@users.noreply.github.com>
2023-08-18 21:39:26 +0900

    feat : 몇 곳에 2열의 card-grid 도입

--- client/src/pages/Home/components/TrendingList.jsx
+++ client/src/pages/Home/components/TrendingList.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function TrendingList({ trendingData }) {
     return (
-        <div>
+        <div className="card-grid">
             {trendingData.map((item, index) => (
                 <div className="card" key={index}>
                     {/* <span>Category: {item.category}</span> */}
